fe1d84f6a7cf1a55fe94c458d4491223edd79a99
Kristoffer Grönlund <krig@koru.se>
2014-06-24 16:53:34 +0200

    some tweaks

--- sqstohugo.py
+++ sqstohugo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
+# vim: set fileencoding=utf-8
 
-import xml.etree.ElementTree as e
+from lxml import etree as e
 import os, sys
 from BeautifulSoup import BeautifulSoup, NavigableString
 
@@ -8,14 +9,22 @@ namespaces = {"content": "http://purl.org/rss/1.0/modules/content/"}
 
 tree = e.parse("squarespace-wordpress-export-06-24-2014.xml")
 root = tree.getroot()
+try:
+    os.mkdir("avsnitt")
+except:
+    pass
 for x in root.findall("./channel/item"):
   title = x.find("title").text
   if title.startswith("attachment-"):
     continue
-  print title.encode("utf-8")
+  title = title.encode("utf-8")
 
   date = x.find("pubDate").text
   alias = x.find("link").text
+  if x.find("status"):
+    is_draft = x.find("status").text == "draft"
+  else:
+    is_draft = False
 
   content = x.find("content:encoded", namespaces=namespaces).text
   soup = BeautifulSoup(content)
@@ -30,28 +39,35 @@ for x in root.findall("./channel/item"):
       for l in i:
         if isinstance(l, NavigableString):
           continue
-        links.append((l.a.contents[0], l.a.get("href")))
-    else:
-      contentstr += i.contents[0] + "\n"
+        try:
+          links.append((l.a.contents[0], l.a.get("href")))
+        except:
+          continue
+    elif i.contents:
+      contentstr += unicode(i.contents[0]) + u"\n"
 
     try:
-      slug = int(title.split(" ")[1])
+      slug = str(int(title.split(" ")[1]))
     except:
       slug = title.replace(" ", "_")
 
-  print "+++"
-  print "draft = false"
-  print "title = \"" + title + "\""
-  print "slug = \"" + str(slug) + "\""
-  print "aliases = [\"" + alias + "\"]"
-  print "categories = [\"avsnitt\"]"
-  print "+++"
-  print ""
-  print contentstr.encode("utf-8")
-  print ""
-  print "Laenkar\n--"
+  while os.path.exists("avsnitt/%s.md" % (slug)):
+    slug += "_"
+
+  outf = open("avsnitt/%s.md" % (slug), "w")
+
+  outf.write("+++\n")
+  outf.write("draft = %s\n" % ("true" if is_draft else "false"))
+  outf.write("title = \"" + title + "\"\n")
+  outf.write("slug = \"" + str(slug) + "\"\n")
+  outf.write("aliases = [\"" + alias + "\"]\n")
+  outf.write("categories = [\"avsnitt\"]\n")
+  outf.write("+++\n\n")
+  outf.write(contentstr.encode("utf-8"))
+  outf.write("\n\n")
+  outf.write("## Länkar\n---\n")
 
   for l in links:
-    print "* [%s](%s)" % (l[0].encode("utf-8"), l[1].encode("utf-8"))
+    outf.write("* [%s](%s)\n" % (l[0].encode("utf-8"), l[1].encode("utf-8")))
 
-  sys.exit(0)
+  outf.close()
